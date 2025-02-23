4deb12176e4d47abdce12a346d20fb2e7d266acb
Axel <Guitaron@TVST-GTN45>
2025-02-23 16:38:19 +0100

    Add project files.

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer(@"");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
---
+++ Data/Entities/ProjectEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class ProjectEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+
+    [Column(TypeName = "date")]
+    public DateTime StartDate { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime EndDate { get; set; }
+
+
+    public int CustomerId { get; set; }
+    public CustomerEntity Customer { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public string UserFirstName { get; set; } = null!;
+    public string UserLastName { get; set; } = null!;
+    public string UserEmail { get; set; } = null!;
+
+    public string ProductName { get; set; } = null!;
+    public int ProductPrice { get; set; }
+    public int TotalPrice { get; set; }
+
+}
+
